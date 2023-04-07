e4bf5ab5d44682557496437407cafc9b9dad179a
Rosdiyanto <77766756+rosdiyanto@users.noreply.github.com>
2023-04-08 03:54:50 +0700

    Update join table dan join grouping.php

--- config-codeigniter/join table dan join grouping.php
+++ config-codeigniter/join table dan join grouping.php
@@ -1,4 +1,18 @@
 <?php
+
+public function gabung()
+{
+    // cara kerja
+    // yaitu menampilkan seluruh data dari tabel left (tbl_pemberhentian atau a) besetda jumlah rows pada table a
+    // dan menggabungkan ke tabel right (tbl_pegawai atau b)
+    // dimana primary key di tbl a dan foreignkey di table b
+    $this->db->select('a.id, b.nip, b.nama, b.gender, a.jenis_pemberhentian, b.jabatan, a.tgl_usulan, a.status');
+    $this->db->from('tbl_pemberhentian as a');
+    $this->db->join('tbl_pegawai as b', 'a.id_pegawai = b.id', 'left');
+    $this->db->order_by('a.id', 'desc');
+    return $this->db->get();
+}
+
 public function join($id)
 {
     $this->db->select('tbl_slider.id, img');
